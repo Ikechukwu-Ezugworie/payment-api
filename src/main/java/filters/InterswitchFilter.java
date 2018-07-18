c3cbda6047991277a53c4385a3556aaa5db24cb7
jgibah <jgibah@byteworks.com.ng>
2018-07-18 14:00:44 +0100

    added logs to filter

--- src/main/java/filters/InterswitchFilter.java
+++ src/main/java/filters/InterswitchFilter.java
@@ -8,6 +8,8 @@ import ninja.FilterChain;
 import ninja.Result;
 import ninja.utils.NinjaProperties;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import utils.Constants;
 import utils.ResponseUtil;
 
@@ -15,6 +17,7 @@ import utils.ResponseUtil;
  * CREATED BY GIBAH
  */
 public class InterswitchFilter implements Filter {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
     @Inject
     BaseDao baseDao;
     @Inject
@@ -24,13 +27,14 @@ public class InterswitchFilter implements Filter {
     public Result filter(FilterChain filterChain, Context context) {
 
         if (ninjaProperties.isDev()) {
+            logger.info("<== Dev mode detected. Allowing all IPs");
             return filterChain.next(context);
         }
 
         String requestIp = context.getRemoteAddr();
 
         if (StringUtils.isBlank(requestIp)) {
-            return ResponseUtil.returnJsonResult(403, "Invalid ip");
+            return ResponseUtil.returnJsonResult(403, "Cannot read request IP address");
         }
 
         String whitelist = baseDao.getSettingsValue(Constants.INTERSWITCH_IPS, "");
@@ -42,12 +46,17 @@ public class InterswitchFilter implements Filter {
         if (whitelist.contains(",")) {
             String[] ips = whitelist.split(",");
             for (String ip : ips) {
+                logger.info(String.format("<== Comparing request IP [%s] to whitelisted IP [%s]", requestIp, ip));
+
                 if (requestIp.equalsIgnoreCase(ip)) {
+                    logger.info(String.format("<== Allowing IP %s", requestIp));
                     return filterChain.next(context);
                 }
             }
         } else {
+            logger.info(String.format("<== Comparing request IP [%s] to whitelisted IP [%s]", requestIp, whitelist));
             if (whitelist.equalsIgnoreCase(requestIp)) {
+                logger.info(String.format("<== Allowing IP %s", requestIp));
                 return filterChain.next(context);
             }
         }
