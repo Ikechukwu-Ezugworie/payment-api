75299b4f33cabc4521efbf05d440a19d49bb7427
jgibah <jgibah@byteworks.com.ng>
2018-10-29 16:59:48 +0100

    added context path

--- src/main/java/filters/ExceptionFilter.java
+++ src/main/java/filters/ExceptionFilter.java
@@ -15,6 +15,7 @@ public class ExceptionFilter implements Filter {
 
     @Override
     public Result filter(FilterChain filterChain, Context context) {
+        context.getSession().put("contextPath", context.getContextPath());
         try {
             return filterChain.next(context);
         } catch (Exception e) {
