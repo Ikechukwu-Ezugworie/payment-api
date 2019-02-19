e9ae94cb650215dd0f55d9a03bd21917f73188bf
Oluwatobi Adenekan <tadenekan@byteworks.com.ng>
2019-02-19 17:51:18 +0100

    Updated Code Base

--- src/main/java/Adapters/JsonRequestBodyConverter.java
+++ src/main/java/Adapters/JsonRequestBodyConverter.java
@@ -13,13 +13,13 @@ import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.nio.charset.Charset;
 
-class GsonRequestBodyConverter<T> implements Converter<T, RequestBody> {
+class JsonRequestBodyConverter<T> implements Converter<T, RequestBody> {
     private static final MediaType MEDIA_TYPE = MediaType.parse("application/json; charset=UTF-8");
     private static final Charset UTF_8 = Charset.forName("UTF-8");
     private final Gson gson;
     private final TypeAdapter<T> adapter;
 
-    GsonRequestBodyConverter(Gson gson, TypeAdapter<T> adapter) {
+    JsonRequestBodyConverter(Gson gson, TypeAdapter<T> adapter) {
         this.gson = gson;
         this.adapter = adapter;
     }
